b76144dcbc5bdeae90db40db10284ff38c6f7b31
Bobby Odian <odianrobert@yahoo.com>
2019-12-19 22:48:09 -0500

    have 5 day cards, icons and conversions not complete, need to sytle and console log

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ var icon = document.querySelector('#icon')
 var humidity = document.querySelector('#humidity')
 var uvIndex = document.querySelector('#uvIndex')
 var windSpeed = document.querySelector('#windSpeed')
-var fiveday = document.querySelector('.fiveDay')
+var fiveday = document.querySelector('#fiveDay')
+
+document.title = 'Weather Dashboard'
 
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -14,7 +16,7 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 var dateTime = date+' '+time;
 document.querySelector('#dateTime').textContent = Date();
 //console.log(date+' '+time)
-
+// var iconValue
 document.querySelector('#button').addEventListener('click', function(){
   event.preventDefault()
   var inputValue = document.querySelector('#inputValue')
@@ -33,7 +35,7 @@ document.querySelector('#button').addEventListener('click', function(){
     
     document.querySelector('#name').textContent = nameValue;
     document.querySelector('#temp').textContent = tempValue + ' Degrees Farenheit';
-    document.querySelector('#icon').textContent = iconValue;
+    document.getElementById('icon').src = iconValue;
     document.querySelector('#humidity').textContent = humidityValue + '% Humidity';
     document.querySelector('#windSpeed').textContent = windSpeedValue + ' MPH Wind Speed';
     
@@ -51,21 +53,127 @@ document.querySelector('#button').addEventListener('click', function(){
 
       var latValue = data['coord']['lat'];
       var lonValue = data['coord']['lon'];
-      console.log(latValue, lonValue)
+      //console.log(data)
 
-      //fetch ('http://api.openweathermap.org/data/2.5/uvi?appid=7f517c5b35eadcb0d489ee411ba832f4&lat={lat}&lon={lon}')
+      // fetch (`http://api.openweathermap.org/data/2.5/uvi?appid=7f517c5b35eadcb0d489ee411ba832f4&lat=${latValue}&lon=${lonValue}`).then(
+      //   data => { console.log(data)
+      //     //var uvValue = data['value'];
+      //   //document.querySelector('#uvIndex').textContent = uvValue;
+      // }
+      //)
        //fetch ('http://api.openweathermap.org/data/2.5/uvi?appid=7f517c5b35eadcb0d489ee411ba832f4&lat=latValue&lon=lonValue')
 
       
-      // var uvValue = data['value'];
-      // document.querySelector('#uvIndex').textContent = uvValue;
+       getIndexVal(latValue,lonValue)
     })
   })
 
+  const getIndexVal = function(lon, lat)
+  {
+    fetch (`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=7f517c5b35eadcb0d489ee411ba832f4`).then(
+        data => data.json()
+         
+      ).then( dataJson => {
+       var uvValue = dataJson['value'];
+        document.querySelector('#uvIndex').textContent = uvValue;
+      
+      }
+      )
+  }
+
+  document.querySelector('#button').addEventListener('click', function(){
+    event.preventDefault()
+
+    var inputValue = document.querySelector('#inputValue')
+    fetch ('https://api.openweathermap.org/data/2.5/forecast?zip='+inputValue.value+'&appid=7f517c5b35eadcb0d489ee411ba832f4')
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data)
+        var fiveDay1 = data.list[4]
+        var dt = fiveDay1['dt']; //var s = new Date(1504095567183).toLocaleDateString("en-US")
+        //var timestamp = moment.unix(1293683278);
+        //var tempValue = Math.round((data['main']['temp'] -273.15)*1.8)+32
+        var temp = fiveDay1['main']['temp']
+        var humidity = fiveDay1['main']['humidity'];
+        var icon = fiveDay1['weather'][icon]
+
+        //console.log(dt, temp, humidity)
+        document.querySelector('#date1').textContent = dt;
+        document.querySelector('#temp1').textContent = temp;
+        document.querySelector('#humidity1').textContent = humidity;
+        document.getElementById('icon1').src = icon;
+        
+        var fiveDay2 = data.list[12]
+        var dt = fiveDay2['dt']; 
+        var temp = fiveDay2['main']['temp']
+        var humidity = fiveDay2['main']['humidity'];
+        var icon = fiveDay2['weather'][icon]
+
+        
+        document.querySelector('#date2').textContent = dt;
+        document.querySelector('#temp2').textContent = temp;
+        document.querySelector('#humidity2').textContent = humidity;
+        document.getElementById('icon2').src = icon;
+        
+        var fiveDay3 = data.list[20]
+        var dt = fiveDay3['dt']; 
+        var temp = fiveDay3['main']['temp']
+        var humidity = fiveDay3['main']['humidity'];
+        var icon = fiveDay3['weather'][icon]
+
+      
+        document.querySelector('#date3').textContent = dt;
+        document.querySelector('#temp3').textContent = temp;
+        document.querySelector('#humidity3').textContent = humidity;
+        document.getElementById('icon3').src = icon;
+
+        var fiveDay4 = data.list[28]
+        var dt = fiveDay4['dt']; 
+        var temp = fiveDay4['main']['temp']
+        var humidity = fiveDay4['main']['humidity'];
+        var icon = fiveDay4['weather'][icon]
+
+        
+        document.querySelector('#date4').textContent = dt;
+        document.querySelector('#temp4').textContent = temp;
+        document.querySelector('#humidity4').textContent = humidity;
+        document.getElementById('icon4').src = icon;
+
+        var fiveDay5 = data.list[36]
+        var dt = fiveDay5['dt']; 
+        var temp = fiveDay5['main']['temp']
+        var humidity = fiveDay5['main']['humidity'];
+        var icon = fiveDay5['weather'][icon]
+
+        //console.log(dt, temp, humidity)
+        document.querySelector('#date5').textContent = dt;
+        document.querySelector('#temp5').textContent = temp;
+        document.querySelector('#humidity5').textContent = humidity;
+        document.getElementById('icon5').src = icon;
+
+        
+  
+  
+  
+      })
+    })
   
 
-
-
+      //Date
+      //Icon image (visual representation of weather conditions)
+      //Temperature
+      //Humidity
+      
+      
+      
+      
+      
+      
+        //4 12 20 28 36
+        //api.openweathermap.org/data/2.5/weather?zip='+inputValue.value+'&appid=7f517c5b35eadcb0d489ee411ba832f4
+  
+  //5day
+  //api.openweathermap.org/data/2.5/forecast?zip={zip code}
 
 //http://openweathermap.org/img/wn/''@2x.png  to convert code to weather icons
 //(0K − 273.15) × 9/5 + 32 = -459.7°F to convert kelvin to farenheit
